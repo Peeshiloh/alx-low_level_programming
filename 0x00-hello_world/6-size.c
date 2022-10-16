673ef3509eb6d76113ee90eea7354dfe5272fad8
Peeshiloh <peeshiloh003@gmail.com>
2022-10-16 13:03:49 -0700

    6th commit message

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main (void) 
+{
+	printf ("size of a char: %lu",sizeof(char));
+	printf ("size of a char: %lu",sizeof(int));
+	printf ("size of a char: %lu",sizeof(long int));
+	printf ("size of a char: %lu",sizeof(long long int));
+	printf ("size of a char: %lu",sizeof(float));
+	return(0);
+}
